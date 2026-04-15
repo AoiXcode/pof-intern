9ffa7b799af75a4ce3cdea335e71e07cad603ce8
JUNGSON\Moderator <dasallajohnammiel@gmail.com>
2026-04-15 17:02:45 +0800

    big update

--- src/Core.js
+++ src/Core.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./assets/css/global.css"
 import {Home} from "./components/home/Index";
@@ -7,14 +8,27 @@ import {Form} from "./components/list//form/Index"
 
 
 const Core= () => {
+    const [form, setForm] = useState({
+    method: 'create', 
+    submitted: false,
+    confirmation: {
+      toggled: false, 
+      type: '',
+    },
+    inputs: {
+      name: { value: '', stat: false, msg: '' },
+      case: { value: 0, stat: false, msg: '' },
+      city: { value: 0, stat: false, msg: '' },
+    }
+  });
 return(
   <div class="Container">
         <Router>
            <Switch>
               <Route exact path="/" render ={(v) => <Home{...v}/>}/>
-              <Route exact path="/statistics" render ={(v) => <Statistics{...v}/>}/>
-              <Route exact path="/list" render ={(v) => <List{...v}/>}/>
-               <Route exact path="/list/form" render ={(v) => <Form{...v}/>}/>
+              <Route exact path="/statistics" render ={(v) => <Statistics{...v} />}/>
+              <Route exact path="/list" render ={(v) => <List{...v} form={form} setForm={setForm}/>}/>
+               <Route exact path="/list/form" render ={(v) => <Form{...v} form={form} setForm={setForm}/>}/>
            </Switch>
         </Router>
         {/* <Home/>
--- src/components/list/form/Index.js
+++ src/components/list/form/Index.js
@@ -1,5 +1,12 @@
 import { Bottom } from "../../navigations/Bottom"
-export const Form =()=>{
+export const Form =({form, setForm})=>{
+    const inputHandler =(e) =>{
+        const {name, value} = e.target
+        const f ={...form}
+        f.inputs[name].value =value
+        
+        setForm(f)
+    }
     return(
         <div className="form-main-container">
             <div className="form-container-1">
@@ -8,11 +15,11 @@ export const Form =()=>{
             <div className="form-container-2">
                 <div>
                     <label>Complete Name</label>
-                    <input type="text" name="name" value=""/>
+                    <input type="text" name="name" value={form.inputs.name.value} onChange={inputHandler}/>
                 </div>
                 <div>
                     <label>Case</label>
-                    <select name="case">
+                    <select name="case" value={form.inputs.case.value} onChange={inputHandler}>
                         <option value={1}>Affected</option>
                         <option value={2}>Death</option>
                         <option value={3}>Recovered</option>
@@ -22,7 +29,7 @@ export const Form =()=>{
                 </div>
                  <div>
                     <label>City</label>
-                    <select name="case">
+                    <select name="case" value={form.inputs.city.value} onChange={inputHandler}>
                         <option value={1}>Makati</option>
                         <option value={2}>Mandaluyong</option>
                         <option value={3}>Manila</option>
@@ -30,6 +37,7 @@ export const Form =()=>{
                         <option value={5}>Taguig</option>
                     </select>
                 </div>
+                 <button>Save Case</button>
             </div>
             <Bottom/>
         </div>
